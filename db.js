8b5555b4b8dd275d18dd38feefedb67ddf72b18d
viveksingh045 <vivekkumarsingh9450@gmail.com>
2025-09-05 11:13:20 +0530

    Update MongoDB connection string and comment out local URL

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 
 
-const mongoURL = "mongodb://localhost:27017/hotels"
+//const mongoURL = "mongodb://localhost:27017/hotels"
 
-mongoose.connect(mongoURL,
-    {useNewUrlParser: true,
+const mongoURL = "mongodb+srv://VKS:Atlas12345@vkscloud.2137i1b.mongodb.net/?retryWrites=true&w=majority&appName=VKSCloud/"
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
     useUnifiedTopology: true
-})
+});
 
 const db = mongoose.connection;
 
